ab8d3c7b740173c1869f9ce5ba36d164ea4393b3
Wisdom ML <108320655+wisdomml2020@users.noreply.github.com>
2022-06-28 20:18:05 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# skin_cancer_detection_mobilenetv2_webapp
-Skin Cancer Detection flask web app in python 
+# Flask web app for Skin Cancer Detection
+
+[![](https://img.shields.io/badge/python-2.7%2C%203.5%2B-green.svg)]()
+
+
+
+------------------
+## About the app
+> This repository was created to help clarify how to utilise flask and gunicorn to easily deploy a python/keras deep learning model as a web app on Azure. This example features code for online deployment of a multi-class medical image classification model, based on convolutional neural network architecture. The CNN has three hidden layers and has been trained on the following  Skin Cancer dataset  <a href="https://challenge.isic-archive.com/data/"> ISIC Skin Cancer Challenge 2019</a>. The trained model achieved accuracy of more than 97% on the test set and its weights have been saved in the Models folder (see file: my_model_v1.h5) in the very useful HDF5 format. You may use your own saved trained model! Just make sure you put it in the Models folder and name it appropriately so that the flask app may call it.
+
+
+
+> This web application has been created and the changes to whatManu Siddhartha  had already prepared were the following:
+<ul>
+
+<li>Inclusion of gunicorn to the requirements.txt file</li>
+<li>Changed the app.py (flask app) file to adapt it to the required functionality according to the trained binary image classification model. The program was also modified to delete every uploaded image after providing the prediction. This will prevent exceeding capacity limits on Heroku servers. The last lines of the file have been modified to work with gunicorn. </li>
+<li>The Index.html and base.html files have been modified accordingly to include references and information about the model</li>
+<li>This README file has been adapted to provide instructions about heroku deployment. The part for customisation has been modified also</li>
+</ul>
+
+> To create a web app that runs locally follow instructions in the README file, to run the model locally. Gevent or gunicorn may be used for local deployment too.
+
+## Customization options
+
+### Use your own model
+
+Place your trained keras deep learning model to the models directory.
+
+
+### Use other pre-trained model
+
+See [Keras applications](https://keras.io/applications/) for more available models such as DenseNet, MobilNet, NASNet, etc.
+
+
+### UI Modification
+
+Modify files in `templates` and `static` directory.
+
+`index.html`, `predict.html` for the UI and `app.py` for all the behaviors
+
+## Deployment
+
+Update soon
+
+
---
+++ app.py
@@ -0,0 +1,107 @@
+﻿"""
+Skin cancer detection web app
+
+designed and developed by Wisdom ML
+"""
+# Importing essential python libraries
+
+from __future__ import division, print_function
+import os
+import numpy as np
+# Keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+# Flask utils
+from flask import Flask,  url_for, request, render_template,send_from_directory
+from werkzeug.utils import secure_filename
+from heatmap import save_and_display_gradcam,make_gradcam_heatmap
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+# Define a flask app
+app = Flask(__name__, static_url_path='')
+
+
+app.config['HEATMAP_FOLDER'] = 'heatmap'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/model_v1.h5'
+
+
+#Load your trained model
+model = load_model(MODEL_PATH)
+        # Necessary to make everything ready to run on the GPU ahead of time
+print('Model loaded. Start serving...')
+
+
+class_dict = {0:"Basal_Cell_Carcinoma (Cancer)",
+             1:"Melanoma (Cancer)",
+             2:"Nevus (Non-Cancerous)"}
+
+@app.route('/uploads/<filename>')
+def upload_img(filename):
+    
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+        
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+
+def model_predict(img_path, model):
+    
+    img = Image.open(img_path).resize((224,224)) #target_size must agree with what the trained model expects!!
+
+    # Preprocessing the image
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img.astype('float32')/255
+   
+    preds = model.predict(img)[0]
+    prediction = sorted(
+      [(class_dict[i], round(j*100, 2)) for i, j in enumerate(preds)],
+      reverse=True,
+      key=lambda x: x[1]
+  )
+    return prediction,img
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the file from post request
+        
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        print(file_path)
+        f.save(file_path)
+        file_name=os.path.basename(file_path)
+        # Make prediction
+        pred,img = model_predict(file_path, model)
+        
+        last_conv_layer_name = "block_16_depthwise"
+        heatmap = make_gradcam_heatmap(img, model, last_conv_layer_name)
+        fname=save_and_display_gradcam(file_path, heatmap)
+        
+        
+        
+    return render_template('predict.html',file_name=file_name, heatmap_file=fname,result=pred)
+
+
+
+
+    #this section is used by gunicorn to serve the app on Azure and localhost
+if __name__ == '__main__':
+        app.run(debug=True, host="localhost", port=8080)
+    #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
+    # Serve the app with gevent 
+    #http_server = WSGIServer(('', 5000), app)
+    #http_server.serve_forever()
